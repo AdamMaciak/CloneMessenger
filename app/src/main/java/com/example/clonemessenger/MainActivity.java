272d8185de6051af27d049a8309db4617b1ee8c8
path1997 <path1997@users.noreply.github.com>
2020-03-30 22:53:05 +0200

    Dodany Clound Messaging

--- app/src/main/java/com/example/clonemessenger/MainActivity.java
+++ app/src/main/java/com/example/clonemessenger/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.clonemessenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
